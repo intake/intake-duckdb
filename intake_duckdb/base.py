989a533931ed05d58bf31e46b541e281aa3124d2
Blake Rosenthal <brosenthal@anaconda.com>
2023-03-28 14:21:21 -0700

    Remove leftover ibis code; fix base.py version

--- intake_duckdb/base.py
+++ intake_duckdb/base.py
@@ -1,7 +1,7 @@
 import numpy as np
 from intake.source import base
 
-__version__ = "0.0.1"
+from . import __version__
 
 
 class DuckDBSource(base.DataSource):
@@ -117,62 +117,6 @@ class DuckDBSource(base.DataSource):
         self._load()
         return self._dataframe
 
-    def to_ibis(self):
-        """
-        Create an ibis expression for the data source.
-        The sql_expr for the source must be a table, not a table expression.
-        The ibis expression is not partitioned.
-        """
-        client, supports_schemas = make_ibis_client(self._uri)
-        schema = self._sql_kwargs.get("schema")
-        schema_kwargs = {"schema": schema} if supports_schemas else {}
-
-        if self._sql_expr not in client.list_tables(**schema_kwargs):
-            # SQLAlchemy-based ibis clients don't currently have
-            # client.sql() implemented.
-            raise ValueError("Only full tables can be used in to_ibis")
-        else:
-            return client.table(self._sql_expr, **schema_kwargs)
-
     def _close(self):
         self._dataframe = None
         self._con.close()
-
-
-def make_ibis_client(uri):
-    """
-    Create an ibis client from a SQLAlchemy connection string.
-
-    Currently targets existing ibis backends that use SQLAlchemy, namely
-        MySQL
-        PostgreSQL
-        SQLite
-
-    Parameters
-    ----------
-    uri: str
-        connection string (sql sqlalchemy documentation)
-
-    Returns
-    -------
-    A tuple of client, supports_schemas
-    """
-    import sqlalchemy
-
-    url = sqlalchemy.engine.url.make_url(uri)
-    dialect = url.get_dialect()
-    name = dialect.name
-    if name == "postgresql":
-        import ibis
-
-        return ibis.postgres.connect(url=uri), True
-    elif name == "mysql":
-        import ibis
-
-        return ibis.mysql.connect(url=uri), True
-    elif name == "sqlite":
-        import ibis
-
-        return ibis.sqlite.connect(path=url.database), False
-    else:
-        raise ValueError(f"Unable to create an ibis connection for {uri}")
